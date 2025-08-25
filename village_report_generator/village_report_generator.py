c07ec86cc8fa8bc5342d7c55d3c90b062260dec2
HernandoMV <hernybiotec@gmail.com>
2025-08-25 17:16:59 +0200

    one more

--- village_report_generator/village_report_generator.py
+++ village_report_generator/village_report_generator.py
@@ -19,31 +19,32 @@ def main():
     if not config_files:
         print("No configuration files found in the active configuration files directory.")
         return
-    if len(config_files) > 1:
-        print("Multiple configuration files found. Please ensure only one is present. To be changed!!")
-        return
-    config_file = config_files[0]  # Assuming only one config file is present
-    print(f"Using configuration file: {config_file}")
-    # Get today's date in YYYY-MM-DD format
-    todays_date = datetime.datetime.now().strftime('%Y-%m-%d')
-    # date will be the previous day
-    date = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-    print(f"Generating report for date: {date}")
-    # Generate the quarto file
-    generate_quarto_file(config_file, date)
+    # if len(config_files) > 1:
+    #     print("Multiple configuration files found. Please ensure only one is present. To be changed!!")
+    #     return
+    # config_file = config_files[0]  # Assuming only one config file is present
+    for config_file in config_files:
+        print(f"Using configuration file: {config_file}")
+        # # Get today's date in YYYY-MM-DD format
+        # todays_date = datetime.datetime.now().strftime('%Y-%m-%d')
+        # date will be the previous day
+        date = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+        print(f"Generating report for date: {date}")
+        # Generate the quarto file
+        generate_quarto_file(config_file, date)
 
-    # Step 2: Render the quarto file to HTML
-    # make sure the quarto file is there
-    config_file_name = Path(config_file).stem
-    quarto_files_path = Path(f'docs/quarto_files/{config_file_name}_{date}.qmd')
-    if not quarto_files_path.exists():
-        print(f"Quarto file {quarto_files_path} does not exist. Please check the configuration.")
-        return
-    
-    # this was for debugging purposes, to be removed later
-    # subprocess.run(['/home/training_village/script_test.sh'], check=True)  # Assuming script_test.sh handles the rendering
+        # Step 2: Render the quarto file to HTML
+        # make sure the quarto file is there
+        config_file_name = Path(config_file).stem
+        quarto_files_path = Path(f'docs/quarto_files/{config_file_name}_{date}.qmd')
+        if not quarto_files_path.exists():
+            print(f"Quarto file {quarto_files_path} does not exist. Please check the configuration.")
+            return
+        
+        # this was for debugging purposes, to be removed later
+        # subprocess.run(['/home/training_village/script_test.sh'], check=True)  # Assuming script_test.sh handles the rendering
 
-    subprocess.run(['quarto', 'render', str(quarto_files_path)], check=True)
+        subprocess.run(['quarto', 'render', str(quarto_files_path)], check=True)
 
     # Step 3: Update the index.html file
     # make sure the quarto report is generated
